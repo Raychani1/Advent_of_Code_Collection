09116ee8ddcd833f9ae1c48c7944ad0d82e58cf6
Raychani1 <lacko9929@gmail.com>
2021-12-01 11:06:46 +0100

    Implement 2021 - Day 01

---
+++ Python/Advent_of_Code_2021/Day_1/aoc_2021_01.py
@@ -0,0 +1,94 @@
+from aocd import submit
+from termcolor import colored
+from aocd.models import Puzzle
+
+
+class AOC2021D01:
+
+    def __init__(self) -> None:
+        """Initialize the AOC2021D01 Class."""
+
+        self.__puzzle = Puzzle(year=2021, day=1)
+        self.__data = self.__read_puzzle_input(
+            puzzle_input=self.__puzzle.input_data
+        )
+
+    @staticmethod
+    def __read_puzzle_input(puzzle_input: str) -> list:
+        """Process puzzle input.
+
+        Args:
+            puzzle_input (str): Puzzle input string
+
+        Returns:
+            list: Processed puzzle input
+
+        """
+
+        return list(map(int, puzzle_input.split('\n')))
+
+    def __solve_puzzle_1(self) -> int:
+        """Solve the first part of the Puzzle.
+
+        Returns:
+            int: Number of measurements that are larger than the previous
+                measurement
+
+        """
+
+        increased: int = 0
+
+        for i in range(1, len(self.__data)):
+            if self.__data[i] - self.__data[i - 1] > 0:
+                increased += 1
+
+        return increased
+
+    def __solve_puzzle_2(self) -> int:
+        """Solve the first part of the Puzzle.
+
+        Returns:
+            int: Number of sums of 3 that are larger than the previous sum of 3
+
+        """
+
+        increased: int = 0
+        previous_window: int = sum(self.__data[0:3])
+
+        for i in range(1, len(self.__data) - 2):
+            current_window: int = sum(self.__data[i:i + 3])
+
+            if current_window - previous_window > 0:
+                increased += 1
+
+            previous_window = current_window
+
+        return increased
+
+    def solve_and_display_puzzles(self) -> None:
+        """Solve both Puzzles and Display the Solution."""
+
+        print(
+            self.__solve_puzzle_1(),
+            colored(
+                'measurements are larger than the previous measurement.',
+                'green'
+            )
+        )
+
+        print(
+            self.__solve_puzzle_2(),
+            colored(
+                'sums are larger than the previous sum.',
+                'green'
+            )
+        )
+
+    def solve_and_submit_puzzles(self) -> None:
+        """Solve both Puzzles and Submit the Solution to the Advent of Code
+            Website.
+
+        """
+
+        submit(year=2021, day=1, part="a", answer=self.__solve_puzzle_1())
+        submit(year=2021, day=1, part="b", answer=self.__solve_puzzle_2())
