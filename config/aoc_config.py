363b0632367d80b0b8c0fc14e5fa706d540a192f
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-12-01 07:27:22 +0100

    Add config based execution

---
+++ config/aoc_config.py
@@ -0,0 +1,10 @@
+from typing import Any, Dict, List
+
+from Advent_of_Code_2022.Day_01.aoc_2022_01 import AOC2022D01
+
+
+AOC_CONFIG: Dict[str, List[Any]] = {
+    '2022': [
+        AOC2022D01()
+    ]
+}
