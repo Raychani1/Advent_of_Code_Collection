4f75643bb767bce3a2b6e4e8808f3c8270948f56
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-12-04 10:24:31 +0100

    Solve AOC 2022 Day 4

--- config/aoc_config.py
+++ config/aoc_config.py
@@ -3,12 +3,14 @@ from typing import Any, Dict, List
 from Advent_of_Code_2022.Day_01.aoc_2022_01 import AOC2022D01
 from Advent_of_Code_2022.Day_02.aoc_2022_02 import AOC2022D02
 from Advent_of_Code_2022.Day_03.aoc_2022_03 import AOC2022D03
+from Advent_of_Code_2022.Day_04.aoc_2022_04 import AOC2022D04
 
 
 AOC_CONFIG: Dict[str, List[Any]] = {
     '2022': [
         AOC2022D01(),
         AOC2022D02(),
-        AOC2022D03()
+        AOC2022D03(),
+        AOC2022D04(),
     ]
 }
