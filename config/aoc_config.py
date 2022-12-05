6dc7648df574c1a1bf5b28e16480512a01c3d72c
Ladislav Rajcsányi <rajcsanyi.ladislav.it@gmail.com>
2022-12-05 08:28:51 +0100

    Solve AOC-2022-05 (#22)

--- config/aoc_config.py
+++ config/aoc_config.py
@@ -4,6 +4,7 @@ from Advent_of_Code_2022.Day_01.aoc_2022_01 import AOC2022D01
 from Advent_of_Code_2022.Day_02.aoc_2022_02 import AOC2022D02
 from Advent_of_Code_2022.Day_03.aoc_2022_03 import AOC2022D03
 from Advent_of_Code_2022.Day_04.aoc_2022_04 import AOC2022D04
+from Advent_of_Code_2022.Day_05.aoc_2022_05 import AOC2022D05
 
 
 AOC_CONFIG: Dict[str, List[Any]] = {
@@ -12,5 +13,6 @@ AOC_CONFIG: Dict[str, List[Any]] = {
         AOC2022D02(),
         AOC2022D03(),
         AOC2022D04(),
+        AOC2022D05(),
     ]
 }
