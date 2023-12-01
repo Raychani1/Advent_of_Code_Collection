10d6f00ab9b5afd7f66262f39e367ae673ce9a7c
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2023-12-01 08:24:12 +0100

    Solve AOC 2023 Day 1

--- config/aoc_config.py
+++ config/aoc_config.py
@@ -10,6 +10,7 @@ from Advent_of_Code_2022.Day_07.aoc_2022_07 import AOC2022D07
 from Advent_of_Code_2022.Day_08.aoc_2022_08 import AOC2022D08
 from Advent_of_Code_2022.Day_09.aoc_2022_09 import AOC2022D09
 from Advent_of_Code_2022.Day_10.aoc_2022_10 import AOC2022D10
+from Advent_of_Code_2023.Day_01.aoc_2023_01 import AOC2023D01
 
 AOC_CONFIG: Dict[str, List[Any]] = {
     '2022': [
@@ -23,5 +24,8 @@ AOC_CONFIG: Dict[str, List[Any]] = {
         AOC2022D08(),
         AOC2022D09(),
         AOC2022D10(),
+    ],
+    '2023': [
+        AOC2023D01(),
     ]
 }
