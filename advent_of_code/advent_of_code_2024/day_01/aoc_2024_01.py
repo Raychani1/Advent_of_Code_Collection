37f08f62b057d6edf429d19cba31eddfd82d069e
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2024-12-01 19:17:54 +0100

    Implement AOC 2024 Day 1

--- advent_of_code/advent_of_code_2024/day_01/aoc_2024_01.py
+++ advent_of_code/advent_of_code_2024/day_01/aoc_2024_01.py
@@ -1,4 +1,5 @@
-from typing import Any
+from collections import Counter
+from typing import List, Tuple
 
 from aocd.models import Puzzle
 from termcolor import colored
@@ -10,40 +11,51 @@ class AOC2024D01:
         self.year: int = 2024
         self.day: int = 1
         self.__puzzle: Puzzle = Puzzle(year=self.year, day=self.day)
-        self.__data: Any = self.__process_puzzle_input()
+        self.__left, self.__right = self.__process_puzzle_input()
 
-    def __process_puzzle_input(self) -> Any:
+    def __process_puzzle_input(self) -> Tuple[List[int], List[int]]:
         """Processes Puzzle Input.
 
         Returns:
-            Any: Processed Puzzle Input.
+            Tuple[List[int], List[int]]: Processed Puzzle Input. Left and Right
+                columns of input.
         """
-        pass
+        individual_numbers = list(
+            map(lambda x: x.split('   '), self.__puzzle.input_data.split('\n'))
+        )
 
-    def solve_puzzle_1(self) -> Any:
+        left = sorted([int(sub_list[0]) for sub_list in individual_numbers])
+        right = sorted([int(sub_list[1]) for sub_list in individual_numbers])
+
+        return left, right
+
+    def solve_puzzle_1(self) -> int:
         """Solves the first part of the Puzzle.
 
         Returns:
-            Any: _description_
+            int: The total distance between the left and right list.
         """
-        pass
+        return sum(
+            [abs(x - y) for x, y in list(zip(self.__left, self.__right))]
+        )
 
-    def solve_puzzle_2(self) -> Any:
+    def solve_puzzle_2(self) -> int:
         """Solves the second part of the Puzzle.
 
         Returns:
-            Any: _description_
+            int: The similarity score of the left and right list.
         """
-        pass
+        occurrence = Counter(self.__right)
+        return sum([x * occurrence[x] for x in self.__left])
 
     def solve_and_display_puzzles(self) -> None:
         """Solves both Puzzles and Display the Solution."""
         print(
-            colored('TODO', 'green'),
+            colored('The total distance between the two columns:', 'green'),
             self.solve_puzzle_1(),
         )
 
         print(
-            colored('TODO', 'green'),
+            colored('The similarity score of the two columns:', 'green'),
             self.solve_puzzle_2(),
         )
--- advent_of_code/config/aoc_config.py
+++ advent_of_code/config/aoc_config.py
@@ -1,3 +1,9 @@
 from typing import Any, Dict, List
 
-AOC_CONFIG: Dict[str, List[Any]] = {}
+from advent_of_code.advent_of_code_2024.day_01.aoc_2024_01 import AOC2024D01
+
+AOC_CONFIG: Dict[str, List[Any]] = {
+    '2024': [
+        AOC2024D01(),
+    ]
+}
